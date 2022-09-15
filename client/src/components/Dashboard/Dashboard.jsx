8d437b620bf2f06a61ea9a4a8559d0f4fca54dd3
gino <gamexgaming1997@gmail.com>
2022-09-15 16:41:51 +0800

    done navigation design

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useRef, useState } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTachometerAlt, FaShoppingCart, FaTable, FaUser, FaTruck, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
 
 // brand img
 import brand from '../../assets/img/brand.png';
@@ -48,26 +48,50 @@ const Dashboard = () => {
                 </div>
                 <div className='navigation'>
                     <div className='category'>
+                        <FaTachometerAlt className='meter' />
                         <span className='text'>
                             Dashboard
                         </span>
                     </div>
-                    <button className='btn-1'>
-
+                    <button className='btn-1' style={{
+                        marginTop: '1.5vh'
+                    }}>
+                        <FaShoppingCart className='cartIcon' />
+                        <span className='text'>
+                            Sales
+                        </span>
                     </button>
                     <button className='btn-1'>
-
+                        <FaTable className='tableIcon' />
+                        <span className='text'>
+                            Products
+                        </span>
                     </button>
                     <button className='btn-1'>
-
+                        <FaUser className='personIcon' />
+                        <span className='text'>
+                            Customer
+                        </span>
                     </button>
                     <button className='btn-1'>
-
+                        <FaTruck className='truckIcon' />
+                        <span className='text'>
+                            Supplier
+                        </span>
                     </button>
                     <button className='btn-1'>
-
+                        <FaChartBar className='chartIcon' />
+                        <span className='text'>
+                            Sales report
+                        </span>
                     </button>
                 </div>
+                <button className='btnLogout'>
+                    <FaSignOutAlt className='signoutIcon' />
+                    <span className='text'>
+                        Logout
+                    </span>
+                </button>
             </nav>
             <div className='dashboard-container' ref={_tabs}>
                 <div className='navContainer' ref={nav_1}>
